a133885bd4c740621e26c10731bbdf2880a776e5
Aaron Chang <changac2@uci.edu>
2018-06-17 23:40:52 -0700

    Moved constant definition to this file and changed define to static const

--- src/cpu.c
+++ src/cpu.c
@@ -4,6 +4,12 @@
 #include <string.h>
 
 
+static const BYTE Z_FLAG = 0x80;
+static const BYTE N_FLAG = 0x40;
+static const BYTE H_FLAG = 0x20;
+static const BYTE C_FLAG = 0x10;
+
+
 CPU *CPU_Create(){
     CPU *cpu = NULL;
     cpu = (CPU *) malloc(sizeof(CPU));
