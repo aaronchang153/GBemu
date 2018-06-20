bb9c413b941a47a0a1f3d5f0911459e446d8428a
Aaron Chang <changac2@uci.edu>
2018-06-20 15:04:36 -0700

    Created a main just for testing if modules are linking properly

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "cpu.h"
+#include "memory.h"
+
+#include <stdio.h>
+
+
+int main(){
+    printf("This main doesn't do anything.\n");
+    printf("It's just to see if the files are linking properly.\n");
+    return 0;
+}
