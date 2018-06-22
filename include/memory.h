07e45875741a2f475548421f562895df16b955d0
Aaron Chang <changac2@uci.edu>
2018-06-21 21:03:05 -0700

    Restructured the memory module

--- include/memory.h
+++ include/memory.h
@@ -3,11 +3,48 @@
 
 #include "common.h"
 
+/*
+ * DMG Memory Map:
+ * > Taken from giibiiadvanced docs
+ * > https://github.com/AntonioND/giibiiadvance/blob/master/docs/TCAGBD.pdf
+ * 
+ * 0000-3FFF: [ROM0] Non-switchable ROM bank
+ *     > 0000-0100: [BOOT] Boot ROM (on startup)
+ * 4000-7FFF: [ROMX] Switchable ROM bank
+ * 8000-9FFF: [VRAM] Video RAM
+ * A000-BFFF: [SRAM] External RAM in cartridge
+ * C000-CFFF: [WRAM0] Work RAM
+ * D000-DFFF: [WRAMX] Work RAM (switchable in GBC)
+ * E000-FDFF: [ECHO] Complicated, but usually just echos WRAM
+ * FE00-FE9F: [OAM] Sprite information table
+ * FEA0-FEFF: <UNUSED>
+ * FF00-FF7F: [IO] I/O Registers
+ * FF80-FFFE: [HRAM] Internal CPU RAM
+ *      FFFF: [IE] Interrupt enable flags
+ * 
+ */
+
+typedef enum{
+    BOOT    = 0x0000,
+    ROM0    = 0x0000,
+    ROMX    = 0x4000,
+    VRAM    = 0x8000,
+    SRAM    = 0xA000,
+    WRAM0   = 0xC000,
+    WRAMX   = 0xD000,
+    ECHO    = 0xE000,
+    OAM     = 0xFE00,
+    UNUSED  = 0xFEA0,
+    IO      = 0xFF00,
+    HRAM    = 0xFF80,
+    IE      = 0xFFFF
+} MEM_REGION;
+
 
 typedef struct{
-    char startup;
-    BYTE *boot;
-    BYTE *mem;
+    bool startup;
+    BYTE boot[0x100];
+    BYTE memory[0x10000];
 } MEMORY;
 
 MEMORY *Mem_Create();
--- src/memory.c
+++ src/memory.c
@@ -4,54 +4,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-static const int BOOT_ROM_SIZE = 0x100;     // 256  B
-static const int MEMORY_SIZE   = 0x10000;   //  64 KB
-
 static const char *BOOT_ROM = "bootstrap.bin";
+
+static MEM_REGION Mem_GetRegion(MEMORY *mem, WORD addr);
 
 
 MEMORY *Mem_Create(){
-    MEMORY *memory = NULL;
-    memory = (MEMORY *) malloc(sizeof(MEMORY));
-    if(memory != NULL){
-        memory->boot = (BYTE *) malloc(BOOT_ROM_SIZE);
-        memory->mem  = (BYTE *) malloc(MEMORY_SIZE);
-        if(memory->boot == NULL || memory->mem == NULL){
-            Mem_Destroy(memory);
-            memory = NULL;
-        }
-        else{
-            Mem_Init(memory);
-        }
-    }
+    MEMORY *memory = malloc(sizeof(MEMORY));
     return memory;
 }
 
 void Mem_Init(MEMORY *mem){
     if(mem != NULL){
-        mem->startup = 1;
-        memset((void *) mem->mem, 0, MEMORY_SIZE);
-        FILE *fp = fopen(BOOT_ROM, "rb");
-        if(fp == NULL){
-            printf("Failed to load boot rom.\n");
-        }
-        else{
-            int i;
-            for(i = 0; i < BOOT_ROM_SIZE; i++){
-                fread((void *) &mem->boot[i], 1, 1, fp);
-            }
-        }
+        memset((void *) mem, 0, sizeof(MEMORY));
     }
 }
 
 void Mem_Destroy(MEMORY *mem){
     if(mem != NULL){
-        if(mem->boot != NULL){
-            free(mem->boot);
-        }
-        if(mem->mem != NULL){
-            free(mem->mem);
-        }
         free(mem);
     }
 }
@@ -74,4 +44,33 @@ void Mem_IncByte(MEMORY *mem, WORD addr){
 
 void Mem_DecByte(MEMORY *mem, WORD addr){
 
+}
+
+static MEM_REGION Mem_GetRegion(MEMORY *mem, WORD addr){
+    if(mem->startup && addr < 0x100)
+        return BOOT;
+    else if(addr == 0xFFFF)
+        return IE;
+    else if(addr >= HRAM)
+        return HRAM;
+    else if(addr >= IO)
+        return IO;
+    else if(addr >= UNUSED)
+        return UNUSED;
+    else if(addr >= OAM)
+        return OAM;
+    else if(addr >= ECHO)
+        return ECHO;
+    else if(addr >= WRAMX)
+        return WRAMX;
+    else if(addr >= WRAM0)
+        return WRAM0;
+    else if(addr >= SRAM)
+        return SRAM;
+    else if(addr >= VRAM)
+        return VRAM;
+    else if(addr >= ROMX)
+        return ROMX;
+    else
+        return ROM0;
 }
