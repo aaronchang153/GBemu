149cbbcd39c7ac19c5b26b911af9162c0ecf5bda
Aaron Chang <changac2@uci.edu>
2018-06-24 15:36:58 -0700

    Added IE_ADDR and rearranged interrupt flags in order of priority

--- include/common.h
+++ include/common.h
@@ -13,6 +13,7 @@
 #define TMA_ADDR    0xFF06
 #define TAC_ADDR    0xFF07
 #define IF_ADDR     0xFF0F
+#define IE_ADDR     0xFFFF
 
 // Commonly used types
 typedef unsigned char BYTE;
@@ -20,13 +21,15 @@ typedef char SIGNED_BYTE;
 typedef unsigned short WORD;
 typedef short SIGNED_WORD;
 
-// Interrupt Enable Flags
-static const BYTE IEF_JOYPAD   = 0x10;
-static const BYTE IEF_SERIAL   = 0x08;
-static const BYTE IEF_TIMER    = 0x04;
-static const BYTE IEF_LCD_STAT = 0x02;
-static const BYTE IEF_VBLANK   = 0x01;
+// Interrupt Flags (Descending Priority)
+static const BYTE IF_VBLANK   = 0x01;
+static const BYTE IF_LCD_STAT = 0x02;
+static const BYTE IF_TIMER    = 0x04;
+static const BYTE IF_SERIAL   = 0x08;
+static const BYTE IF_JOYPAD   = 0x10;
 
 static inline bool TEST_BIT(BYTE reg, int bit) { return (reg & (0x01 << bit)) != 0x00; }
+
+static inline bool TEST_FLAG(BYTE reg, BYTE flag) { return (reg & flag) == flag; }
 
 #endif // COMMON_H
