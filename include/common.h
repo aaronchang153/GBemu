2f2e40dbea54ce4c9a4359ee6f26d07e27223f9b
Aaron Chang <changac2@uci.edu>
2018-06-23 21:08:31 -0700

    Renamed constants and added TEST_BIT

--- include/common.h
+++ include/common.h
@@ -7,11 +7,11 @@
 #define CYCLE_F 1048576 // Hz
 
 // Important addresses in memory
-#define DIV_ADDR            0xFF04
-#define TIMA_COUNTER_ADDR   0xFF05
-#define TIMA_MODULO_ADDR    0xFF06
-#define TAC_ADDR            0xFF07
-#define IF_ADDR             0xFF0F
+#define DIV_ADDR    0xFF04
+#define TIMA_ADDR   0xFF05
+#define TMA_ADDR    0xFF06
+#define TAC_ADDR    0xFF07
+#define IF_ADDR     0xFF0F
 
 // Commonly used types
 typedef unsigned char BYTE;
@@ -25,5 +25,7 @@ static const BYTE IEF_SERIAL   = 0x08;
 static const BYTE IEF_TIMER    = 0x04;
 static const BYTE IEF_LCD_STAT = 0x02;
 static const BYTE IEF_VBLANK   = 0x01;
+
+static inline bool TEST_BIT(BYTE reg, int bit) { return (reg & (0x01 << bit)) != 0x00; }
 
 #endif // COMMON_H
