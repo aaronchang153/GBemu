901deabfb555a7603da2849134bc0c1395263bf2
Aaron Chang <changac2@uci.edu>
2018-06-24 16:42:49 -0700

    Got rid of unnecessary if-else block

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,8 +1,8 @@
 #include "interrupt.h"
 #include "memory.h"
 
-static void ServiceInterrupt(CPU *c, BYTE type);
 
+static void ServiceInterrupt(CPU *c, WORD service_routine);
 
 void Interrupt_Handle(CPU *c){
     if(c->IME){
@@ -11,36 +11,27 @@ void Interrupt_Handle(CPU *c){
         if(requests != 0 && enable != 0){
             // If there's an interrupt to handle, handle it and disable further interrupts
             if(TEST_FLAG(requests, IF_VBLANK) && TEST_FLAG(enable, IF_VBLANK)){
-                ServiceInterrupt(c, IF_VBLANK);
+                ServiceInterrupt(c, VBLANK_ROUTINE);
             }
             else if(TEST_FLAG(requests, IF_LCD_STAT) && TEST_FLAG(enable, IF_LCD_STAT)){
-                ServiceInterrupt(c, IF_LCD_STAT);
+                ServiceInterrupt(c, LCD_STAT_ROUTINE);
             }
             else if(TEST_FLAG(requests, IF_TIMER) && TEST_FLAG(enable, IF_TIMER)){
-                ServiceInterrupt(c, IF_TIMER);
+                ServiceInterrupt(c, TIMER_ROUTINE);
             }
             else if(TEST_FLAG(requests, IF_JOYPAD) && TEST_FLAG(enable, IF_JOYPAD)){
-                ServiceInterrupt(c, IF_JOYPAD);
+                ServiceInterrupt(c, JOYPAD_ROUTINE);
             }
             c->IME = false;
         }
     }
 }
 
-static void ServiceInterrupt(CPU *c, BYTE type){
+static void ServiceInterrupt(CPU *c, WORD service_routine){
     // Push pc to stack
     Mem_WriteByte(c->memory, c->sp - 1, (c->pc & 0xFF00) >> 8);
     Mem_WriteByte(c->memory, c->sp - 2, (c->pc & 0x00FF));
     c->sp -= 2;
-
-    if(type == IF_VBLANK)
-        c->pc = VBLANK_ROUTINE;
-    else if(type == IF_LCD_STAT)
-        c->pc = LCD_STAT_ROUTINE;
-    else if(type == IF_TIMER)
-        c->pc = TIMER_ROUTINE;
-    else if(type == IF_JOYPAD)
-        c->pc = JOYPAD_ROUTINE;
-
+    c->pc = service_routine;
     c->cycles += 3 * 4;
 }
