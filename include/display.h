7d2b465ae254d1c7941a27fe2aabb4926fb40e7a
Aaron Chang <changac2@uci.edu>
2018-06-27 15:54:05 -0700

    Created display module

---
+++ include/display.h
@@ -0,0 +1,32 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "common.h"
+#include "SDL.h"
+
+#define SCREEN_WIDTH  160
+#define SCREEN_HEIGHT 144
+
+#define WINDOW_NAME "GBemu"
+
+#define PIXEL_SIZE 4
+
+typedef struct{
+    BYTE r;
+    BYTE g;
+    BYTE b;
+} PIXEL;
+
+typedef struct{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+} DISPLAY;
+
+
+DISPLAY *Display_Create();
+
+void Display_Destroy(DISPLAY *d);
+
+void Display_RenderScreen(DISPLAY *d, PIXEL frame[SCREEN_WIDTH][SCREEN_HEIGHT]);
+
+#endif // DISPLAY_H
---
+++ src/display.c
@@ -0,0 +1,53 @@
+#include "display.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+DISPLAY *Display_Create(){
+    DISPLAY *display = malloc(sizeof(DISPLAY));
+    if(display != NULL){
+        display->window = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                SCREEN_WIDTH * PIXEL_SIZE, SCREEN_HEIGHT * PIXEL_SIZE, SDL_WINDOW_SHOWN);
+        if(display->window == NULL){
+            printf("Unable to create window.\n");
+            Display_Destroy(display);
+            display = NULL;
+        }
+        else{
+            display->renderer = SDL_CreateRenderer(display->window, -1, SDL_RENDERER_ACCELERATED);
+            if(display->renderer == NULL){
+                printf("Unable to create renderer.\n");
+                Display_Destroy(display);
+                display = NULL;
+            }
+        }
+    }
+    return display;
+}
+
+void Display_Destroy(DISPLAY *d){
+    if(d != NULL){
+        SDL_DestroyRenderer(d->renderer);
+        SDL_DestroyWindow(d->window);
+        free(d);
+    }
+}
+
+void Display_RenderScreen(DISPLAY *d, PIXEL frame[SCREEN_WIDTH][SCREEN_HEIGHT]){
+    int i, j;
+    SDL_Rect pixel;
+
+    for(j = 0; j < SCREEN_HEIGHT; j++){
+        for(i = 0; i < SCREEN_WIDTH; i++){
+            SDL_SetRenderDrawColor(d->renderer, frame[i][j].r, frame[i][j].g, frame[i][j].b, 0xFF);
+            pixel.x = i * PIXEL_SIZE;
+            pixel.y = j * PIXEL_SIZE;
+            pixel.w = PIXEL_SIZE;
+            pixel.h = PIXEL_SIZE;
+
+            SDL_RenderFillRect(d->renderer, &pixel);
+        }
+    }
+    SDL_RenderPresent(d->renderer);
+}
