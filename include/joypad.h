ac3a38030bfa03bfb96d938e3b4b40fa10251c3d
Aaron Chang <changac2@uci.edu>
2018-06-27 18:11:04 -0700

    Created joypad module

---
+++ include/joypad.h
@@ -0,0 +1,35 @@
+#ifndef JOYPAD_H
+#define JOYPAD_H
+
+#include "common.h"
+#include "SDL.h"
+
+#define JOYPAD_PRESSED      0
+#define JOYPAD_NOT_PRESSED  1
+
+typedef struct{
+    union{
+        struct{
+            BYTE a      : 1;
+            BYTE b      : 1;
+            BYTE start  : 1;
+            BYTE select : 1;
+            BYTE right  : 1;
+            BYTE left   : 1;
+            BYTE up     : 1;
+            BYTE down   : 1;
+        };
+        BYTE state;
+    };
+} JOYPAD;
+
+JOYPAD *Joypad_Create();
+
+void Joypad_Destroy(JOYPAD *j);
+
+BYTE Joypad_GetState(JOYPAD *j, BYTE p1);
+
+// Returns true if there's a Joypad Interrupt and false otherwise
+bool Joypad_SetState(JOYPAD *j, SDL_Event e, int state, BYTE p1);
+
+#endif // JOYPAD_H
---
+++ src/joypad.c
@@ -0,0 +1,67 @@
+#include "joypad.h"
+
+#include <stdlib.h>
+
+
+static int MapKeyPress(SDL_Event e);
+
+JOYPAD *Joypad_Create(){
+    JOYPAD *joypad = malloc(sizeof(JOYPAD));
+    if(joypad != NULL){
+        joypad->state = 0xFF;
+    }
+    return joypad;
+}
+
+void Joypad_Destroy(JOYPAD *j){
+    if(j != NULL){
+        free(j);
+    }
+}
+
+BYTE Joypad_GetState(JOYPAD *j, BYTE p1){
+    if(TEST_BIT(p1, 4))
+        return (p1 & 0xF0) | ((j->state & 0xF0) >> 4);
+    else if(TEST_BIT(p1, 5))
+        return (p1 & 0xF0) | (j->state & 0x0F);
+    else
+        return p1;
+}
+
+bool Joypad_SetState(JOYPAD *j, SDL_Event e, int state, BYTE p1){
+    bool interrupt = false;
+    int key = MapKeyPress(e);
+    if(key >= 0){
+        if(state == JOYPAD_PRESSED && TEST_BIT(j->state, key)){
+            // If the key is going from unpressed to pressed
+            if(((key > 3) && TEST_BIT(p1, 4)) || ((key >= 3) && TEST_BIT(p1, 5)))
+                interrupt = true;
+        }
+        // Clear corresponding bit if pressed, otherwise set it to 1
+        j->state = (state == JOYPAD_PRESSED) ? (j->state & ~(0x01 << key)) : (j->state | (0x01 << key));
+    }
+    return interrupt;
+}
+
+static int MapKeyPress(SDL_Event e){
+    switch(e.key.keysym.scancode){
+        case SDL_SCANCODE_J: // A
+            return 0;
+        case SDL_SCANCODE_K: // B
+            return 1;
+        case SDL_SCANCODE_B: // START
+            return 2;
+        case SDL_SCANCODE_V: // SELECT
+            return 3;
+        case SDL_SCANCODE_D: // RIGHT
+            return 4;
+        case SDL_SCANCODE_A: // LEFT
+            return 5;
+        case SDL_SCANCODE_W: // UP
+            return 6;
+        case SDL_SCANCODE_S: // DOWN
+            return 7;
+        default:
+            return -1;
+    };
+}
