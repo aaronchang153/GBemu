236b7e7ccc9d7157f3f69cf436839b8cdfc544fd
Aaron Chang <changac2@uci.edu>
2018-06-29 16:18:36 -0700

    Clear interrupt flag when servicing the interrupt

--- src/interrupt.c
+++ src/interrupt.c
@@ -11,19 +11,24 @@ void Interrupt_Handle(CPU *c){
         if(requests != 0 && enable != 0){
             // If there's an interrupt to handle, handle it and disable further interrupts
             if(TEST_FLAG(requests, IF_VBLANK) && TEST_FLAG(enable, IF_VBLANK)){
+                requests &= ~(IF_VBLANK); // Clear corresponding flag
                 ServiceInterrupt(c, VBLANK_ROUTINE);
             }
             else if(TEST_FLAG(requests, IF_LCD_STAT) && TEST_FLAG(enable, IF_LCD_STAT)){
+                requests &= ~(IF_LCD_STAT); // Clear corresponding flag
                 ServiceInterrupt(c, LCD_STAT_ROUTINE);
             }
             else if(TEST_FLAG(requests, IF_TIMER) && TEST_FLAG(enable, IF_TIMER)){
+                requests &= ~(IF_TIMER); // Clear corresponding flag
                 ServiceInterrupt(c, TIMER_ROUTINE);
             }
             else if(TEST_FLAG(requests, IF_JOYPAD) && TEST_FLAG(enable, IF_JOYPAD)){
+                requests &= ~(IF_JOYPAD); // Clear corresponding flag
                 ServiceInterrupt(c, JOYPAD_ROUTINE);
             }
             c->IME = false;
         }
+        Mem_WriteByte(c->memory, IF_ADDR, requests);
     }
 }
 
