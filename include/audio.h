9ad4a45e85d9b0b90dbfaf1c5ad3e73939cc3f1a
Aaron Chang <changac2@uci.edu>
2018-07-15 20:25:23 -0700

    Created audio module and implemented the basic functions

---
+++ include/audio.h
@@ -0,0 +1,69 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include "common.h"
+#include "memory.h"
+
+#include <stdint.h>
+#include <SDL2/SDL.h>
+
+/**
+ * The GameBoy has 4 sound channels:
+ *  1. Square wave with sweep and envelope functions
+ *  2. Square wave with envelope function
+ *  3. Arbitrary waveform from wave pattern RAM
+ *  4. White noise with evelope function
+ */
+
+#define NR10_ADDR 0xFF10
+#define NR11_ADDR 0xFF11
+#define NR12_ADDR 0xFF12
+#define NR13_ADDR 0xFF13
+#define NR14_ADDR 0xFF14
+
+#define NR21_ADDR 0xFF16
+#define NR22_ADDR 0xFF17
+#define NR23_ADDR 0xFF18
+#define NR24_ADDR 0xFF19
+
+#define NR30_ADDR 0xFF1A
+#define NR31_ADDR 0xFF1B
+#define NR32_ADDR 0xFF1C
+#define NR33_ADDR 0xFF1D
+#define NR34_ADDR 0xFF1E
+
+#define NR41_ADDR 0xFF20
+#define NR42_ADDR 0xFF21
+#define NR43_ADDR 0xFF22
+#define NR44_ADDR 0xFF23
+
+#define NR50_ADDR 0xFF24 // Channel Control + On-Off + Volume
+#define NR51_ADDR 0xFF25 // Sound ouput terminal select
+#define NR52_ADDR 0xFF26 // Sound on/off
+
+#define WAVE_PATTERN_RAM 0xFF30 // $FF30 - $FF3F
+
+#define AUDIO_BUFFER_SIZE 0x800
+
+typedef struct{
+    SDL_AudioSpec audio_spec;
+    int16_t audio_buffer[AUDIO_BUFFER_SIZE]; // samples stored as {left-channel, right-channel}
+    int sample_number; // current spot in audio_buffer
+    int sample_timer; // only sample audio when this timer reaches 0 (cycles)
+    int sound_timer[4]; // in cycles
+    uint8_t sequence[2]; // Both square wave channels have 8 states depending on the duty
+    int frame_countdown[2]; // frame_timer is set to this value when it hits 0
+    int frame_timer[2]; // the corresponding square wave sequence is incremented when this hits 0
+    MEMORY *memory;
+} APU;
+
+
+APU *APU_Create();
+
+void APU_Destroy(APU *a);
+
+void APU_SetMemory(APU *a, MEMORY *mem);
+
+void APU_Update(APU *a, int cycles);
+
+#endif // AUDIO_H
---
+++ src/audio.c
@@ -0,0 +1,119 @@
+#include "audio.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+#define SAMPLE_FREQUENCY     44100 // Hz
+#define FRAME_SEQUENCER_FREQ   512 // Hz
+
+#define MAX_VOLUME 2000
+
+const int CYCLES_PER_SAMPLE = CLK_F / SAMPLE_FREQUENCY;
+const int CYCLES_PER_FRAME  = CLK_F / FRAME_SEQUENCER_FREQ;
+
+static const int16_t square_wave_table[4][8] = 
+{   // Same type as the audio data buffer and audio spec format
+    {-1,  1,  1,  1,  1,  1,  1,  1}, // 12.5% duty
+    {-1, -1,  1,  1,  1,  1,  1,  1}, // 25% duty
+    {-1, -1, -1, -1,  1,  1,  1,  1}, // 50% duty
+    {-1, -1, -1, -1, -1, -1,  1,  1}  // 75% duty
+};
+
+static inline int us_to_cycles(float us) { return (int) round(us * CLK_F); }
+
+static void Update_Ch1(APU *a, int cycles); // Square wave with sweep and envelope
+static void Update_Ch2(APU *a, int cycles); // Square wave with envelope
+static void Update_Ch3(APU *a, int cycles); // Arbitrary waveform
+static void Update_Ch4(APU *a, int cycles); // White noise
+
+APU *APU_Create(){
+    APU *apu = malloc(sizeof(APU));
+    if(apu != NULL){
+        memset(apu, 0, sizeof(APU));
+        SDL_AudioSpec want;
+        want.freq = SAMPLE_FREQUENCY;
+        want.format = AUDIO_S16;
+        want.channels = 2; // Stereo Sound
+        want.callback = NULL;
+        SDL_OpenAudio(&want, &apu->audio_spec);
+        if(apu->audio_spec.freq != want.freq || apu->audio_spec.format != want.format){
+            APU_Destroy(apu);
+            apu = NULL;
+        }
+        else{
+            apu->sample_timer = CYCLES_PER_SAMPLE;
+            apu->sequence[0] = apu->sequence[1] = 0;
+            srand(time(NULL)); // Random number needed for noise on channel 4
+        }
+    }
+    return apu;
+}
+
+void APU_Destroy(APU *a){
+    SDL_CloseAudio();
+    if(a != NULL){
+        free(a);
+    }
+}
+
+void APU_SetMemory(APU *a, MEMORY *mem){
+    if(a != NULL){
+        a->memory = mem;
+    }
+}
+
+void APU_Update(APU *a, int cycles){
+    if(a != NULL){
+        a->sample_timer -= cycles;
+        if(a->sample_timer <= 0){
+            a->sample_timer += CYCLES_PER_SAMPLE;
+            a->audio_buffer[a->sample_number] = 0;
+            a->audio_buffer[a->sample_number + 1] = 0;
+
+            Update_Ch1(a, cycles);
+            Update_Ch2(a, cycles);
+            Update_Ch3(a, cycles);
+            Update_Ch4(a, cycles);
+            
+            a->sample_number += 2;
+            if(a->sample_number == AUDIO_BUFFER_SIZE){
+                SDL_QueueAudio(1, a->audio_buffer, AUDIO_BUFFER_SIZE);
+                a->sample_number = 0;
+            }
+        }
+    }
+}
+
+static void Update_Ch1(APU *a, int cycles){
+    /***** Incomplete *****/
+    int frequency = Mem_ReadByte(a->memory, NR13_ADDR);
+    frequency |= (Mem_ReadByte(a->memory, NR14_ADDR) & 0x7) << 8;
+    a->frame_countdown[0] = (2048 - frequency) * 4;
+    a->frame_timer[0]--;
+    if(a->frame_timer == 0){
+        a->frame_timer[0] = a->frame_countdown[0];
+        a->sequence[0] = (a->sequence[0] + 1) % 8;
+    }
+}
+
+static void Update_Ch2(APU *a, int cycles){
+    int frequency = Mem_ReadByte(a->memory, NR23_ADDR);
+    frequency |= (Mem_ReadByte(a->memory, NR24_ADDR) & 0x7) << 8;
+    a->frame_countdown[1] = (2048 - frequency) * 4;
+    int wave_duty = Mem_ReadByte(a->memory, NR21_ADDR) >> 6;
+    a->frame_timer[1]--;
+    if(a->frame_timer == 0){
+        a->frame_timer[1] = a->frame_countdown[1];
+        a->sequence[1] = (a->sequence[1] + 1) % 8;
+    }
+}
+
+static void Update_Ch3(APU *a, int cycles){
+
+}
+
+static void Update_Ch4(APU *a, int cycles){
+
+}
