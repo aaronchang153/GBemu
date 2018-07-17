b5496e9509f162f522c665478ecf1a5f34ee1b15
Aaron Chang <changac2@uci.edu>
2018-07-16 17:36:45 -0700

    Implemented a bit more of channel 1 and 2. Something resembling music now plays

--- src/audio.c
+++ src/audio.c
@@ -8,17 +8,21 @@
 #define SAMPLE_FREQUENCY     44100 // Hz
 #define FRAME_SEQUENCER_FREQ   512 // Hz
 
-#define MAX_VOLUME 2000
+// Volume needs to be normalized to [-1.0, 1.0] when using F32 audio format
+#define MAX_VOLUME 20
+#define MAX_CHANNEL_VOLUME (MAX_VOLUME / 4)
+
+#define NORMALIZE_SAMPLE(sample) (sample /= MAX_VOLUME)
 
 const int CYCLES_PER_SAMPLE = CLK_F / SAMPLE_FREQUENCY;
 const int CYCLES_PER_FRAME  = CLK_F / FRAME_SEQUENCER_FREQ;
 
-static const float square_wave_table[4][8] = 
+static const AudioSample square_wave_table[4][8] = 
 {   // Same type as the audio data buffer and audio spec format
-    {-1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0}, // 12.5% duty
-    {-1.0, -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0}, // 25% duty
-    {-1.0, -1.0, -1.0, -1.0,  1.0,  1.0,  1.0,  1.0}, // 50% duty
-    {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0,  1.0,  1.0}  // 75% duty
+    {-1,  1,  1,  1,  1,  1,  1,  1}, // 12.5% duty
+    {-1, -1,  1,  1,  1,  1,  1,  1}, // 25% duty
+    {-1, -1, -1, -1,  1,  1,  1,  1}, // 50% duty
+    {-1, -1, -1, -1, -1, -1,  1,  1}  // 75% duty
 };
 
 static inline int us_to_cycles(float us) { return (int) round(us * CLK_F); }
@@ -33,9 +37,14 @@ APU *APU_Create(){
     if(apu != NULL){
         memset(apu, 0, sizeof(APU));
         SDL_AudioSpec want;
+#ifdef FLOAT32_AUDIO
+        want.format = AUDIO_F32SYS;
+#else
+        want.format = AUDIO_S16SYS;
+#endif // FLOAT32_AUDIO
         want.freq = SAMPLE_FREQUENCY;
-        want.format = AUDIO_F32LSB;
         want.channels = 2; // Stereo Sound
+        want.samples = AUDIO_BUFFER_LENGTH;
         want.callback = NULL;
         SDL_OpenAudio(&want, &apu->audio_spec);
         if(apu->audio_spec.freq != want.freq || apu->audio_spec.format != want.format){
@@ -46,6 +55,7 @@ APU *APU_Create(){
             apu->sample_timer = CYCLES_PER_SAMPLE;
             apu->sequence[0] = apu->sequence[1] = 0;
             srand(time(NULL)); // Random number needed for noise on channel 4
+            SDL_PauseAudio(0);
         }
     }
     return apu;
@@ -69,17 +79,22 @@ void APU_Update(APU *a, int cycles){
         a->sample_timer -= cycles;
         if(a->sample_timer <= 0){
             a->sample_timer += CYCLES_PER_SAMPLE;
-            a->audio_buffer[a->sample_number] = 0.0;
-            a->audio_buffer[a->sample_number + 1] = 0.0;
+            a->audio_buffer[a->sample_number] = 0;
+            a->audio_buffer[a->sample_number + 1] = 0;
 
             Update_Ch1(a, cycles);
             Update_Ch2(a, cycles);
             Update_Ch3(a, cycles);
             Update_Ch4(a, cycles);
+
+#ifdef FLOAT32_AUDIO
+            NORMALIZE_SAMPLE(a->audio_buffer[a->sample_number]);
+            NORMALIZE_SAMPLE(a->audio_buffer[a->sample_number + 1]);
+#endif // FLOAT32_AUDIO
             
             a->sample_number += 2;
             if(a->sample_number == AUDIO_BUFFER_LENGTH){
-                SDL_QueueAudio(1, a->audio_buffer, AUDIO_BUFFER_LENGTH);
+                SDL_QueueAudio(1, a->audio_buffer, AUDIO_BUFFER_LENGTH * sizeof(AudioSample));
                 a->sample_number = 0;
             }
         }
@@ -88,26 +103,41 @@ void APU_Update(APU *a, int cycles){
 
 static void Update_Ch1(APU *a, int cycles){
     /***** Incomplete *****/
-    int frequency = Mem_ReadByte(a->memory, NR13_ADDR);
+    int frequency, wave_duty;
+
+    frequency = Mem_ReadByte(a->memory, NR13_ADDR);
     frequency |= (Mem_ReadByte(a->memory, NR14_ADDR) & 0x7) << 8;
     a->frame_countdown[0] = (2048 - frequency) * 4;
-    a->frame_timer[0]--;
-    if(a->frame_timer == 0){
-        a->frame_timer[0] = a->frame_countdown[0];
+
+    wave_duty = Mem_ReadByte(a->memory, NR11_ADDR) >> 6;
+
+    a->frame_timer[0] -= cycles;
+    if(a->frame_timer[0] <= 0){
+        a->frame_timer[0] = a->frame_countdown[1];
         a->sequence[0] = (a->sequence[0] + 1) % 8;
     }
+
+    a->audio_buffer[a->sample_number] = MAX_CHANNEL_VOLUME * square_wave_table[wave_duty][a->sequence[0]];
+    a->audio_buffer[a->sample_number + 1] = MAX_CHANNEL_VOLUME * square_wave_table[wave_duty][a->sequence[0]];
 }
 
 static void Update_Ch2(APU *a, int cycles){
-    int frequency = Mem_ReadByte(a->memory, NR23_ADDR);
+    int frequency, wave_duty;
+
+    frequency = Mem_ReadByte(a->memory, NR23_ADDR);
     frequency |= (Mem_ReadByte(a->memory, NR24_ADDR) & 0x7) << 8;
     a->frame_countdown[1] = (2048 - frequency) * 4;
-    int wave_duty = Mem_ReadByte(a->memory, NR21_ADDR) >> 6;
-    a->frame_timer[1]--;
-    if(a->frame_timer == 0){
+
+    wave_duty = Mem_ReadByte(a->memory, NR21_ADDR) >> 6;
+
+    a->frame_timer[1] -= cycles;
+    if(a->frame_timer[1] <= 0){
         a->frame_timer[1] = a->frame_countdown[1];
         a->sequence[1] = (a->sequence[1] + 1) % 8;
     }
+
+    a->audio_buffer[a->sample_number] = MAX_CHANNEL_VOLUME * square_wave_table[wave_duty][a->sequence[1]];
+    a->audio_buffer[a->sample_number + 1] = MAX_CHANNEL_VOLUME * square_wave_table[wave_duty][a->sequence[1]];
 }
 
 static void Update_Ch3(APU *a, int cycles){
